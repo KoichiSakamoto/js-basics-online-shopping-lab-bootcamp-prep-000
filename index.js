24b265a968f34e31ae213f4c05f9d2242b02a72f
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-02 20:36:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,6 +65,6 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
   var returnString = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
-  return returnString;
   cart = [];
+  return returnString;
 }
